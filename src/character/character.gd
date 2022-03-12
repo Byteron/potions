1d8385d88adfc6913af9926edf647fd33d6d30af
Aaron Winter <winter.aaron93@gmail.com>
2022-03-12 12:03:12 +0100

    controller

---
+++ src/character/character.gd
@@ -0,0 +1,15 @@
+extends Node
+
+@onready var parent := get_parent()
+@export var speed := 2
+
+
+func _process(delta: float) -> void:
+	parent.position += get_direction() * speed * delta
+
+
+func get_direction() -> Vector3:
+	var direction := Vector3()
+	direction.x = Input.get_axis("ui_left", "ui_right")
+	direction.z = Input.get_axis("ui_up", "ui_down")
+	return direction.normalized()
