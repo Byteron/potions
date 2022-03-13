56c03b0853f3b797d196c69d9cdb71c66ebf209b
Aaron Winter <winter.aaron93@gmail.com>
2022-03-13 10:10:25 +0100

    anim

--- src/character/character.gd
+++ src/character/character.gd
@@ -4,16 +4,19 @@ class_name Character
 var _item: Item = null
 
 @onready var _item_container: Position3D = $ItemContainer
+@onready var _anim: AnimationPlayer = $AnimationPlayer
 
 
 func take_item(item: Item) -> void:
 	_item = item
 	_item_container.add_child(item)
+	_anim.play("carry")
 
 
 func drop_item() -> void:
 	_item.queue_free()
 	_item = null
+	_anim.play("normal")
 
 
 func has_item() -> bool:
