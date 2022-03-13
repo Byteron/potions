02216172e4416eaaff47ee1e3a949492f23111f5
Aaron Winter <winter.aaron93@gmail.com>
2022-03-13 12:09:41 +0100

    stage animation

--- src/growable/growable.gd
+++ src/growable/growable.gd
@@ -15,19 +15,29 @@ var is_grown := false
 
 func _ready() -> void:
 	_next_stage()
-
+	
 
 func _next_stage() -> void:
 	_stage += 1
 
 	if data.stages.size() > _stage:
 		_sprite.texture = data.stages[_stage]
-	
+
 	is_grown = data.stages.size() == _stage + 1
 	
 	if not is_grown:
 		_timer.start()
+	
+	animate()
+
+
+func animate() -> void:
+	var tween = get_tree().create_tween()
+	_sprite.scale = Vector3(0.7, 0.7, 0.7)
+	tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK).tween_property(_sprite, "scale", Vector3(1, 1, 1), 0.2)
+	tween.play()
 
 
 func _on_timer_timeout() -> void:
 	_next_stage()
+
