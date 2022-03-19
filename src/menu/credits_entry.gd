8fcc7043b29f775bfce358451807ed8f7069cec4
Aaron Winter <winter.aaron93@gmail.com>
2022-03-19 17:58:26 +0100

    credits

---
+++ src/menu/credits_entry.gd
@@ -0,0 +1,16 @@
+extends VBoxContainer
+class_name CreditsEntry
+
+var score := ""
+var roles := ""
+
+@onready var name_label: Label = $Name
+@onready var score_label: Label = $Score
+@onready var roles_label: Label = $Roles
+
+
+func _ready() -> void:
+	name_label.text = name
+	score_label.text = score
+	roles_label.text = roles
+
--- src/menu/victory_screen.gd
+++ src/menu/victory_screen.gd
@@ -1,9 +1,37 @@
 extends Panel
 class_name VictoryScreen
 
+@export var CreditsEntry: PackedScene = null
+
 @onready var score_label: Label = $VBoxContainer/CenterContainer/VBoxContainer/ScoreLabel
 @onready var stats_label: Label = $VBoxContainer/CenterContainer/VBoxContainer/StatsLabel
 @onready var back_button: Button = $BackButton
+
+@onready var credits_container: Control = $Credits
+
+
+var credit_entries := [
+	{
+		name = "Aaron Winter",
+		score = "18300",
+		roles = "Game Design and Programming",
+	},
+	{
+		name = "Zowie van Dillen",
+		score = "15600",
+		roles = "2D and 3D Art",
+	},
+	{
+		name = "Ramita",
+		score = "???",
+		roles = "Sound Effects",
+	},
+	{
+		name = "Vikfro",
+		score = "???",
+		roles = "3D Art and Music",
+	},
+]
 
 var score := 0
 var sold := 0
@@ -11,18 +39,33 @@ var failed := 0
 
 
 func _ready() -> void:
+	_init_credits()
+
 	back_button.visible = false
 	var tween = get_tree().create_tween()
 	tween.set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT).tween_property(self, "score", Recipes.score, 3).set_delay(0.25)
 	tween.set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT).tween_property(self, "sold", Recipes.sold, 1).set_delay(0.25)
 	tween.set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_OUT).tween_property(self, "failed", Recipes.failed, 0.5).set_delay(0.25)
-	tween.tween_callback(back_button.show)
+	
+	for entry in credits_container.get_children():
+		tween.set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_OUT).tween_property(entry, "modulate:a", 1.0, 0.5).set_delay(0.5)
+
+	tween.tween_callback(back_button.show).set_delay(0.5)
 
 
 func _process(delta: float) -> void:
 	score_label.text = "Score: " + str(score)
 	stats_label.text = "Sold: " + str(sold) + "\n" + "Failed: " + str(failed)
 
+
+func _init_credits() -> void:
+	for dict in credit_entries:
+		var entry: CreditsEntry = CreditsEntry.instantiate()
+		entry.modulate.a = 0
+		entry.name = dict.name
+		entry.score = dict.score
+		entry.roles = dict.roles
+		credits_container.add_child(entry)
 
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene("res://src/menu/title_screen.tscn")
