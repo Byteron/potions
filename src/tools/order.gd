a040a200122048cb90a4aa2bceafee090a8184c5
Aaron Winter <winter.aaron93@gmail.com>
2022-04-10 09:27:42 +0200

    balancing

--- src/tools/order.gd
+++ src/tools/order.gd
@@ -6,11 +6,14 @@ signal finished(order: Order, position: Vector3)
 
 var recipe: Recipe
 
+var time_step := 1.0
+
 @onready var timer: Timer = $Timer
 
 
 func start(time: float) -> void:
 	timer.start(time)
+	time_step = time / 4.0
 
 
 func finish(position: Vector3) -> void:
@@ -18,7 +21,7 @@ func finish(position: Vector3) -> void:
 
 
 func get_time_left_modifier() -> float:
-	return 1.0 + timer.time_left / 25.0
+	return 1.0 + timer.time_left / time_step
 
 
 func _on_timer_timeout() -> void:
